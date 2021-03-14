e72450a1f7523cda7bf3b2cbb58544a9ff1e2b08
egorgsv <egorspb3@gmail.com>
2021-03-14 17:30:02 +0300

    пытаюсь добавить ограничение по памяти

--- main.py
+++ main.py
@@ -1,40 +1,65 @@
+import resource
+import sys
 import nltk
 import pandas as pd
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
 
-nltk.download()
+#nltk.download()
 
-truedf = pd.read_csv('data/True.csv')
-print(truedf.head())
+def memory_limit(maxsize):
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    resource.setrlimit(resource.RLIMIT_DATA, (maxsize, maxsize))
 
-tokens = [word_tokenize(text) for text in truedf['text']]
+def get_memory():
+    with open('/proc/meminfo', 'r') as mem:
+        free_memory = 0
+        for i in mem:
+            sline = i.split()
+            if str(sline[0]) in ('MemFree:', 'Buffers:', 'Cached:'):
+                free_memory += int(sline[1])
+    return free_memory
 
-words = []
-for document in tokens:
-    words_in_text = [word for word in document if word.isalpha()]
-    words.append(words_in_text)
-print(words)
+def main():
+    truedf = pd.read_csv('data/True.csv')
+    print(truedf.head())
 
-# stemming of words
-porter = PorterStemmer()
-stemmed = []
-for document in words:
-    steemmed_words = [porter.stem(word) for word in document]
-    stemmed.append(steemmed_words)
-print(stemmed)
+    tokens = [word_tokenize(text) for text in truedf['text']]
 
-tuples = []
-for i in range(len(stemmed)):
-    for word in stemmed[i]:
-        tuples.append((word, i))
-print(tuples)
+    words = []
+    for document in tokens:
+        words_in_text = [word for word in document if word.isalpha()]
+        words.append(words_in_text)
+    print(words)
 
-terms = {}
-for term, DOCid in tuples:
-    terms.setdefault(term, set()).add(DOCid)
-for term in terms:
-    terms[term] = sorted(terms[term])
-print(terms)
+    # stemming of words
+    porter = PorterStemmer()
+    stemmed = []
+    for document in words:
+        steemmed_words = [porter.stem(word) for word in document]
+        stemmed.append(steemmed_words)
+    print(stemmed)
 
-print(terms['putin'])
+    tuples = []
+    for i in range(len(stemmed)):
+        for word in stemmed[i]:
+            tuples.append((word, i))
+    print(tuples)
+
+    terms = {}
+    for term, DOCid in tuples:
+        terms.setdefault(term, set()).add(DOCid)
+    for term in terms:
+        terms[term] = sorted(terms[term])
+    print(terms)
+
+    print(terms['putin'])
+
+if __name__ == '__main__':
+    memory_limit(1024*1024) # Limitates maximun memory usage to half
+    try:
+        main()
+    except MemoryError:
+        sys.stderr.write('\n\nERROR: Memory Exception\n')
+        sys.exit(1)
+
