538f89c18a36a49a3e90888084709ccab7ba401d
Nick <Nikitoskova123@gmail.com>
2021-03-16 14:45:55 +0300

    added cli

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import resource
 import sys
 import nltk
+import argparse
 import pandas as pd
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
 
-#nltk.download()
+from src.utils import *
+
+# nltk.download()
+
 
 def memory_limit(maxsize):
     soft, hard = resource.getrlimit(resource.RLIMIT_AS)
     resource.setrlimit(resource.RLIMIT_AS, (maxsize, maxsize))
+
 
 def get_memory():
     with open('/proc/meminfo', 'r') as mem:
@@ -20,9 +25,9 @@ def get_memory():
                 free_memory += int(sline[1])
     return free_memory
 
+
 def main():
     truedf = pd.read_csv('data/True.csv')
-    print(truedf.head())
 
     tokens = [word_tokenize(text) for text in truedf['text']]
 
@@ -30,7 +35,6 @@ def main():
     for document in tokens:
         words_in_text = [word for word in document if word.isalpha()]
         words.append(words_in_text)
-    print(words)
 
     # stemming of words
     porter = PorterStemmer()
@@ -38,27 +42,29 @@ def main():
     for document in words:
         steemmed_words = [porter.stem(word) for word in document]
         stemmed.append(steemmed_words)
-    print(stemmed)
 
     tuples = []
     for i in range(len(stemmed)):
         for word in stemmed[i]:
             tuples.append((word, i))
-    print(tuples)
 
     terms = {}
     for term, DOCid in tuples:
         terms.setdefault(term, set()).add(DOCid)
     for term in terms:
         terms[term] = sorted(terms[term])
-    print(terms)
 
-    print(terms['putin'])
 
 if __name__ == '__main__':
-    memory_limit(1024*1024) # Limitates maximun memory usage to half
+    memory_limit(1024*1024)  # Limitates maximum memory usage to half
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('QUERY', type=str)
+
+    args = parser.parse_args()
+
     try:
-        main()
+        print(reversed_polish_notation(str(args.QUERY)))
     except MemoryError:
         sys.stderr.write('\n\nERROR: Memory Exception\n')
         sys.exit(1)
